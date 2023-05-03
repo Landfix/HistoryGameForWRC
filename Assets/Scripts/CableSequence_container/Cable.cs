086e6163fb60d4074e1fba8c21c7d57a18e4b446
Ivan Popenya <54358431+Landfix@users.noreply.github.com>
2023-05-03 17:41:02 +0300

    upgrade cable sequence

--- Assets/Scripts/CableSequence_container/Cable.cs
+++ Assets/Scripts/CableSequence_container/Cable.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
 using LineSequence_container.Configs;
 using UnityEngine;
 
@@ -6,22 +8,34 @@ namespace LineSequence_container
 {
     public class Cable : MonoBehaviour
     {
+        private const int MinSortingOrder = 2;
+        private const int MaxSortingOrder = 10;
+        
         [SerializeField] private SpriteRenderer _model;
+        [SerializeField] private Collider2D _collider2D;
         
         private CableType _cableType;
-        private bool _isConnect;
-
-        private Camera _camera;
+        private Vector2 _savedPosition;
         private Vector2 _startPos;
         private Vector2 _dragOffset;
+        private int _index; 
+        private bool _isMouseDrag;
 
-        public CableType CableType => _cableType;
+        private Camera _camera;
+        private CableBlock _cableBlock;
 
-        public event Action<Cable> Switched;
+        public Collider2D Collider2D => _collider2D;
+        public Vector2 StartPos => _startPos;
+        public bool IsCorrectIndex => _index == (int)_cableType;
 
-        public void Initialize(Camera camera, CableBlock cableBlock, CableConfig cableConfig)
+        public event Action<Cable, Vector2> Up;
+
+        public void Initialize(Camera camera, CableBlock cableBlock, CableConfig cableConfig, int index)
         {
+            _index = index;
+            _cableBlock = cableBlock;
             var position = cableBlock.transform.position;
+            
             _startPos = position;
             transform.position = position;
 
@@ -29,26 +43,54 @@ namespace LineSequence_container
             _model.sprite = cableConfig.Model;
             _cableType = cableConfig.CableType;
         }
-        
+
         private void OnMouseDown()
         {
+            _model.sortingOrder = MaxSortingOrder;
             _dragOffset = (Vector2)transform.position - GetMousePosition();
         }
 
         private void OnMouseDrag()
         {
+            _isMouseDrag = true;
             transform.position = GetMousePosition() + _dragOffset;
         }
 
         private void OnMouseUp()
         {
-            transform.position = _startPos;
+            if (_isMouseDrag)
+            {
+                Up?.Invoke(this, GetMousePosition());
+                _isMouseDrag = false;
+            }
+            else
+                transform.position = _startPos;
+            
+            _model.sortingOrder = MinSortingOrder;
         }
 
         private Vector2 GetMousePosition()
         {
             Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
             return mousePosition;
+        }
+
+        public void SetNewBlock(CableBlock cableBlock, int index)
+        {
+            _index = index;
+            _startPos = _cableBlock.transform.position;
+            _cableBlock = cableBlock;
+        }
+
+        public void Move(Vector2 pos)
+        {
+            transform.DOKill();
+            transform.DOMove(pos,1f).SetEase(Ease.Linear).OnComplete(() => _startPos = pos);
+        }
+
+        public CableBlock GetCableBlock()
+        {
+            return _cableBlock;
         }
     }
 }
--- Assets/Scripts/CableSequence_container/CableSequence.cs
+++ Assets/Scripts/CableSequence_container/CableSequence.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using LineSequence_container.Configs;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = System.Random;
 
 namespace LineSequence_container
@@ -22,8 +25,10 @@ namespace LineSequence_container
     public class CableSequence : MonoBehaviour
     {
         [SerializeField] private CableConfigs _cableConfigs;
-        [Header("Components")]
-        [SerializeField] private List<CableBlock> _cableBlocks;
+
+        [Header("Components")] [SerializeField]
+        private List<CableBlock> _cableBlocks;
+
         [SerializeField] private Cable _cablePrefab;
         [SerializeField] private Transform _cableContainer;
 
@@ -36,15 +41,49 @@ namespace LineSequence_container
             _camera = Camera.main;
             _cables = new List<Cable>();
             CreateCableTypes();
-            
+
             for (int i = 0; i < _cableTypes.Count; i++)
             {
                 CableConfig cableConfig = _cableConfigs.Cables.FirstOrDefault(x => x.CableType == _cableTypes[i]);
-                Cable cable = Instantiate(_cablePrefab,_cableContainer);
-                _cableBlocks[i].Initialize(i+1);
-                cable.Initialize(_camera,_cableBlocks[i],cableConfig);
+                Cable cable = Instantiate(_cablePrefab, _cableContainer);
+                _cableBlocks[i].Initialize(i + 1);
+                cable.Initialize(_camera, _cableBlocks[i], cableConfig,i);
+                cable.Up += Up;
                 _cables.Add(cable);
             }
+        }
+
+        private void Up(Cable currentCable, Vector2 pos)
+        {
+            Cable cable = _cables.FirstOrDefault(x => x.Collider2D.OverlapPoint(pos) && x != currentCable);
+
+            if (!cable)
+            {
+                currentCable.Move(currentCable.StartPos);
+                return;
+            }
+            
+            SwapCables(currentCable, cable);
+
+            if (_cables.All(x => x.IsCorrectIndex)) 
+                StartCoroutine(AllConnectedCoroutine());
+        }
+
+        private void SwapCables(Cable currentCable, Cable cable)
+        {
+            int fromIndex = _cables.IndexOf(currentCable);
+            int toIndex = _cables.IndexOf(cable);
+
+            _cables[fromIndex] = cable;
+            _cables[toIndex] = currentCable;
+
+            CableBlock savedCableBlock = cable.GetCableBlock();
+            cable.SetNewBlock(currentCable.GetCableBlock(),fromIndex);
+            currentCable.SetNewBlock(savedCableBlock,toIndex);
+
+
+            currentCable.Move(cable.StartPos);
+            cable.Move(currentCable.StartPos);
         }
 
         private void CreateCableTypes()
@@ -58,6 +97,16 @@ namespace LineSequence_container
             var rnd = new Random();
             var shuffledCableTypes = _cableTypes.OrderBy(a => rnd.Next());
             _cableTypes = shuffledCableTypes.ToList();
+        }
+        
+        private IEnumerator AllConnectedCoroutine()
+        {
+            // todo recast
+            yield return new WaitForSeconds(1f);
+            for (int i = 0; i < _cables.Count; i++) 
+                _cables[i].Up -= Up;
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
